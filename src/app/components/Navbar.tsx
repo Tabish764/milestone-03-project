db0f7353ce46d686e2ff93061e9f4b5f689d58c4
Tabish Raza <ts764raza@gmail.com>
2025-01-03 17:58:07 +0500

    first commit

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,9 +5,9 @@ const Navbar = () => {
   return (
     <div className=" border-b-2 shadow-xl shadow-red-500/50 backdrop-blur-md    border-red-500 py-4 flex items-center px-4 w-full"> 
 
-        <h1 className="text-red-500 text-2xl">RenderBoost</h1>
-      <div className=" max-w-7xl font-bold mx-auto justify-center items-center text-lg w-full md:flex hidden">
-        <div className="flex gap-5 text-lg items-center">
+        <h1 className="text-red-500 text-sm md:text-2xl">RenderBoost</h1>
+      <div className=" max-w-7xl font-bold mx-auto justify-end items-center text-lg w-full flex">
+        <div className="flex gap-5 text-sm md:text-lg items-center">
           <Link className="hover:text-red-500 hover:delay-100 ease-in-out" href={'/'}>Home</Link>
           <Link className="hover:text-red-500 hover:delay-100 ease-in-out" href="/products">Products</Link>
           <Link className="hover:text-red-500 hover:delay-100 ease-in-out" href="/cart">Cart</Link>
